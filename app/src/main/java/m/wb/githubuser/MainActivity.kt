08ca7036510d791ec247ae0ae387f15f777e32c4
Mukti Wibowo <muktiwbowo@gmail.com>
2023-05-11 22:24:20 +0800

    ui: create app bar

--- app/src/main/java/m/wb/githubuser/MainActivity.kt
+++ app/src/main/java/m/wb/githubuser/MainActivity.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import m.wb.githubuser.ui.theme.GithubUserTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    PageHome()
                 }
             }
         }
@@ -30,14 +32,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun PageHome() {
+    /** show edit text and list of users here **/
+    Scaffold(topBar = {
+        TopAppBar() {
+            Text(
+                text = "Github User",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(horizontal = 18.dp)
+            )
+        }
+    }) {
+        /** show edit text and list of users here **/
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     GithubUserTheme {
-        Greeting("Android")
+        PageHome()
     }
 }
